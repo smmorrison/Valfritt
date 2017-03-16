407406cb4661d903b3ba9f3e5161045865e3a5cd
samanthamorrison <samantha.morrison@hotmail.se>
2017-03-16 14:16:50 +0100

    We have movement

--- WhaleFree/GameScene.swift
+++ WhaleFree/GameScene.swift
@@ -18,30 +18,73 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    
     override func sceneDidLoad() {
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "backgroundImage")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(background)
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(spawnWhale),
+                SKAction.wait(forDuration: 1.0)])))
+        
+        /*var levelTimerLabel = SKLabelNode(fontNamed: "ArialMT")
+        
+        var levelTimerValue: Int = 30 {
+            didSet {
+                levelTimerLabel.text = "Time left: \(levelTimerValue)"
+            }
+        }
+        levelTimerLabel.fontColor = SKColor.black
+        levelTimerLabel.fontSize = 20
+        levelTimerLabel.position = CGPoint(x: size.width/2, y: size.height/2 + 200)
+        levelTimerLabel.text = "Time left: \(levelTimerValue)"
+        addChild(levelTimerLabel)
+        
+        let wait = SKAction.wait(forDuration: 1.0)
+        let block = SKAction.run({
+            [unowned self] in
+            
+            if self.levelTimerValue > 0 {
+                self.levelTimerValue -= 1
+            }else{
+                self.removeAction(forKey: "countdown")
+            }
+        })
+        let sequence = SKAction.sequence([wait,block])
+        
+        run(SKAction.repeatForever(sequence), withKey: "countdown")*/
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func spawnWhale() {
+        let whale = SKSpriteNode(imageNamed: "whale")
+        whale.name = "whale"
+        whale.position = CGPoint(x: frame.size.width + whale.size.width/2,
+                                 y: frame.size.height * random(min: 0, max: 1))
+        addChild(whale)
+        whale.run(
+            SKAction.moveBy(x: -size.width - whale.size.width, y: 0.0,
+                             duration: TimeInterval(random(min: 1, max: 2))))
+    }
+    
+    /*func levelCountdown(){
+        levelTimerValue--
+        levelTimerLabel.text = String(levelTimerValue)
+    }*/
+
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -71,6 +114,7 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if action(forKey: "countdown") != nil {removeAction(forKey: "countdown")}
         }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
